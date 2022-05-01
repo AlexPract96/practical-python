87be5af3ff49c124b3a6a78d84af6fe5e3e20d83
AlexPract96 <104701001+AlexPract96@users.noreply.github.com>
2022-04-30 20:53:52 -0700

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
-#
+# A rubber ball is dropped from a height of 100 meters and each time it hits the ground, 
+# it bounces back up to 3/5 the height it fell. 
+# Write a program bounce.py that prints a table showing the height of the first 10 bounces.
 # Exercise 1.5
+
+height_start = 100.0;
+
+for i in range(1,11):
+    height_start = round(height_start *0.60,4);
+    print(i,height_start);
+    
